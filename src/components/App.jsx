924051c428cbb81b45584ad45742e0e1a656519e
Danylo Altukhov <dan.97198@gmail.com>
2022-10-04 14:26:32 +0300

    upd files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,27 @@
+import { Route, Routes, Link } from "react-router-dom";
+
+
 export const App = () => {
-  
+  return (
+    <div>
+      <nav>
+        <Link to='/'>
+          Home
+        </Link>
+        <Link to="/movies">
+          Movies
+        </Link>
+      </nav>
+
+    <Routes> 
+    <Route path="/" element />
+    <Route path="/movies" element/>
+    <Route path="/movies/:movieId" element >
+      <Route path="cast" element />
+      <Route path="reviews" element/>
+    </Route>
+    
+  </Routes>
+    </div>
+  )
 };
--- src/components/services/filmApi.js
+++ src/components/services/filmApi.js
@@ -1,1 +1,75 @@
+import axios from 'axios';
+
 const KEY = '4f759a8f2a3c2252d280f73946c6d093';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+export const searchMovies = async searchQuery => {
+  const url = `/search/movie`;
+  const urlParams = {
+    api_key: KEY,
+    query: searchQuery,
+  };
+
+  try {
+    const res = await axios.get(url, { params: urlParams });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getTrends = async () => {
+  const url = `/trending/movie/day`;
+  const urlParams = {
+    api_key: KEY,
+  };
+
+  try {
+    const res = await axios.get(url, { params: urlParams });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieDetails = async movieId => {
+  const url = `/movie/${movieId}`;
+  const urlParams = {
+    api_key: KEY,
+  };
+
+  try {
+    const res = await axios.get(url, { params: urlParams });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieCast = async movieId => {
+  const url = `/movie/${movieId}/credits`;
+  const urlParams = {
+    api_key: KEY,
+  };
+
+  try {
+    const res = await axios.get(url, { params: urlParams });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieReviews = async movieId => {
+  const url = `/movie/${movieId}/reviews`;
+  const urlParams = {
+    api_key: KEY,
+  };
+
+  try {
+    const res = await axios.get(url, { params: urlParams });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
